96f73291fde795fb279bc3541a17f542818dee8c
Quathar <99633210+Quathar@users.noreply.github.com>
2023-10-23 19:49:51 +0200

    Change Solution to private

--- src/test/java/com/metrica/codewars/SolutionTest.java
+++ src/test/java/com/metrica/codewars/SolutionTest.java
@@ -76,7 +76,7 @@ class SolutionTest {
                 .toString();
     }
 
-    @RepeatedTest(15000)
+    @RepeatedTest(500)
     void randomEasyModeTest() {
         String phrase = generateString(0, 100);
         String key    = generateString(0, 17);
@@ -84,7 +84,7 @@ class SolutionTest {
         assertEquals(Solution.decode(phrase, key), Kata.decode(phrase, key));
     }
 
-    @RepeatedTest(1000)
+    @RepeatedTest(250)
     void randomHardModeTest() {
         String phrase = generateString(1000, 15000);
         String key    = generateString(10, 17);
@@ -99,91 +99,91 @@ class SolutionTest {
         assertEquals(Solution.encode(phrase, key), Kata.encode(phrase, key));
         assertEquals(Solution.decode(phrase, key), Kata.decode(phrase, key));
     }
-
-}
-
-class Solution {
-
-    // <<-CONSTANT->>
-    private static final int INTERMEDIATE_OFFSET = 'a' - 'Z' - 1;
     
-    // <<-METHODS->>
-    private static int getLastDigit(final int digit) {
-        return digit % 10;
-    }
+    private static class Solution {
 
-    private static List<Integer> getEncryptationOffsets(String key) {
-        return key.chars().map(n -> getLastDigit(n) + key.length()).boxed().toList();
-    }
-
-    private static List<Integer> getPhraseChars(String phrase) {
-        return phrase.chars().boxed().collect(Collectors.toList());
-    }
-
-    private static String parseChars(List<Integer> traduction) {
-        return traduction.stream()
-                .map(n -> (char) n.intValue())
-                .map(c -> Character.toString(c))
-                .collect(Collectors.joining());
-    }
-
-    public static String encode(String phrase, String key) {
-        if (phrase == null || phrase.isEmpty()) return "";
-        if (key    == null || key   .isEmpty()) return phrase;
-
-        List<Integer> offsets     = getEncryptationOffsets(key);
-        List<Integer> phraseChars = getPhraseChars(phrase);
-        List<Integer> traduction  = IntStream.range(0, phrase.length())
-                .mapToObj(i -> {
-                    int keyIndex = i % offsets.size();
-                    int current  = phraseChars.get(i);
-                    int result   = current + offsets.get(keyIndex);
-                    
-                    if (current <= 'Z' && result >= 'a') {
-                        int offset = result - 'Z';
-                        result = 'a' + offset - 1;
-                    }
-
-                    if (result > 'Z' && result < 'a') {
-                        result += INTERMEDIATE_OFFSET;
-                    } else if (result > 'z') {
-                        result = result - 'z' + 'A' - 1;
-                    }
-                    
-                    return result;
-                }).collect(Collectors.toList());
-
-        return parseChars(traduction);
-    }
-
-    public static String decode(String phrase, String key) {
-        if (phrase == null || phrase.isEmpty()) return "";
-        if (key    == null || key   .isEmpty()) return phrase;
+        // <<-CONSTANT->>
+        private static final int INTERMEDIATE_OFFSET = 'a' - 'Z' - 1;
         
-        List<Integer> offsets     = getEncryptationOffsets(key);
-        List<Integer> phraseChars = getPhraseChars(phrase);
-        List<Integer> traduction  = IntStream.range(0, phrase.length())
-                .mapToObj(i -> {
-                    int keyIndex = i % offsets.size();
-                    int current  = phraseChars.get(i);
-                    int result   = current - offsets.get(keyIndex);
-                    
-                    if (current >= 'a' && result <= 'Z') {
-                        int offset = 'a' - result;
-                        result = 'Z' - offset + 1;
-                    }
+        // <<-METHODS->>
+        private static int getLastDigit(final int digit) {
+            return digit % 10;
+        }
 
-                    if (result < 'a' && result > 'Z') {
-                        result -= INTERMEDIATE_OFFSET;
-                    } else if (result < 'A') {
-                        int offset = 'A' - result;
-                        result = 'z' - offset + 1;
-                    }
-                    
-                    return result;
-                }).collect(Collectors.toList());
+        private static List<Integer> getEncryptationOffsets(String key) {
+            return key.chars().map(n -> getLastDigit(n) + key.length()).boxed().toList();
+        }
 
-        return parseChars(traduction);
+        private static List<Integer> getPhraseChars(String phrase) {
+            return phrase.chars().boxed().collect(Collectors.toList());
+        }
+
+        private static String parseChars(List<Integer> traduction) {
+            return traduction.stream()
+                    .map(n -> (char) n.intValue())
+                    .map(c -> Character.toString(c))
+                    .collect(Collectors.joining());
+        }
+
+        public static String encode(String phrase, String key) {
+            if (phrase == null || phrase.isEmpty()) return "";
+            if (key    == null || key   .isEmpty()) return phrase;
+
+            List<Integer> offsets     = getEncryptationOffsets(key);
+            List<Integer> phraseChars = getPhraseChars(phrase);
+            List<Integer> traduction  = IntStream.range(0, phrase.length())
+                    .mapToObj(i -> {
+                        int keyIndex = i % offsets.size();
+                        int current  = phraseChars.get(i);
+                        int result   = current + offsets.get(keyIndex);
+                        
+                        if (current <= 'Z' && result >= 'a') {
+                            int offset = result - 'Z';
+                            result = 'a' + offset - 1;
+                        }
+
+                        if (result > 'Z' && result < 'a') {
+                            result += INTERMEDIATE_OFFSET;
+                        } else if (result > 'z') {
+                            result = result - 'z' + 'A' - 1;
+                        }
+                        
+                        return result;
+                    }).collect(Collectors.toList());
+
+            return parseChars(traduction);
+        }
+
+        public static String decode(String phrase, String key) {
+            if (phrase == null || phrase.isEmpty()) return "";
+            if (key    == null || key   .isEmpty()) return phrase;
+            
+            List<Integer> offsets     = getEncryptationOffsets(key);
+            List<Integer> phraseChars = getPhraseChars(phrase);
+            List<Integer> traduction  = IntStream.range(0, phrase.length())
+                    .mapToObj(i -> {
+                        int keyIndex = i % offsets.size();
+                        int current  = phraseChars.get(i);
+                        int result   = current - offsets.get(keyIndex);
+                        
+                        if (current >= 'a' && result <= 'Z') {
+                            int offset = 'a' - result;
+                            result = 'Z' - offset + 1;
+                        }
+
+                        if (result < 'a' && result > 'Z') {
+                            result -= INTERMEDIATE_OFFSET;
+                        } else if (result < 'A') {
+                            int offset = 'A' - result;
+                            result = 'z' - offset + 1;
+                        }
+                        
+                        return result;
+                    }).collect(Collectors.toList());
+
+            return parseChars(traduction);
+        }
+        
     }
-    
+
 }
